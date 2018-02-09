72066973d36c26b8d2a585dd8c323396406d8d8e
Titouan Van Belle <titouan.vanbelle@gmail.com>
2018-02-09 12:30:21 +0100

    Initial Commit

---
+++ iRecorder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iRecorder
+//
+//  Created by Titouan Van Belle on 09.02.18.
+//  Copyright © 2018 Fyber GmbH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
